18744dafe90a2f9dddecb981686205dae9fad3fb
gabrielles2019 <32675207+gabrielles2019@users.noreply.github.com>
2018-11-27 18:32:01 -0400

    Update Cow.java

--- Cow.java
+++ Cow.java
@@ -1,3 +1,8 @@
+public void setup() {
+  Cow c = new Cow("cow","moo");
+  System.out.printIn(c.getType()+ "goes" + c.getSound());
+}
+
 class Cow implements Animal {     
     private String myType, mySound;
     public Cow(String type, String sound) {
